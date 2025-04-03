e9f9d9b2092511d06801f4d2a0cf43cfa4c285b1
unknown <T.Treillon@lprs.fr>
2025-04-03 19:35:16 +0200

    [feat]Remplissage Suppression et methode

--- structure/Fonctionalité/fonct_suppression.php
+++ structure/Fonctionalité/fonct_suppression.php
@@ -1,3 +1,17 @@
 <?php
 require_once '../bdd/a__roport.sql';
 require_once '../../visuelle/Pages principales/Page de suppression.html';
+require_once '../Modèle/Utilisateurs.php';
+
+session_start();
+$utilisateurs = new Utilisateurs([
+    "email" => $_SESSION['email'],
+]);
+$utilisateurRepository = new UtilisateursRepository();
+
+$resultat = $utilisateurRepository->suppUtilisateurs($utilisateurs);
+
+if($resultat){
+    header("Location: ../../vue/ConnexionEM.php");
+    exit();
+}
