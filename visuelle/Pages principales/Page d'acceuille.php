7404cd876c22bbbc6ec3324da8367f8edc1610c7
Tom TREILLON <TREILLON_To@lprs.local>
2025-04-04 12:00:22 +0200

    [fix]Changement des pages html en php

---
+++ visuelle/Pages principales/Page d'acceuille.php
@@ -0,0 +1,10 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Aéroport Majestueux</title>
+    <link href="../Pages css/Css principale.css">
+</head>
+<body>
+<h1>Aéroport Majestueux</h1>
+</body>
+</html>
